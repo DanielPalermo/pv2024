dadff3818803bd1e48e63e8b9f67b5b933603a30
DanielPalermo <110257968+DanielPalermo@users.noreply.github.com>
2024-04-05 20:58:16 -0300

    Actualizo clase Prueba.java

--- Proyecto1_P/src/ar/edu/unju/pv/ejemplo1/Prueba.java
+++ Proyecto1_P/src/ar/edu/unju/pv/ejemplo1/Prueba.java
@@ -7,7 +7,7 @@ public class Prueba {
 	public static void main(String[] args) {
 		Scanner teclado = new Scanner(System.in);
 		int anio = 2028;
-		String saludo ="Hola Pv";
+		String saludo ="Hola Programacion visual";
 		System.out.println(saludo + " " + anio);
 		
 		if(anio <= 2024 ) {
